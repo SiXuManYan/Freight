cd42ca52dab1c06a2c0746f0356f09190dcb3a5f
wangshiwei <itshiwei@aliyun.com>
2020-11-04 10:43:18 +0800

    refactor:订单UI

--- education-student/src/main/java/com/ftacloud/student/ui/order/list/child/OrderChildHolder.kt
+++ education-student/src/main/java/com/ftacloud/student/ui/order/list/child/OrderChildHolder.kt
@@ -1,6 +1,5 @@
 package com.ftacloud.student.ui.order.list.child
 
-import android.os.SystemClock
 import android.view.View
 import android.view.ViewGroup
 import com.blankj.utilcode.util.StringUtils
@@ -8,9 +7,7 @@ import com.bumptech.glide.Glide
 import com.ftacloud.student.R
 import com.ftacloud.student.frames.entity.Order
 import com.sugar.library.frames.BaseItemViewHolder
-import com.sugar.library.ui.view.countdown.CountDownTextView
 import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.item_home_class_experience.*
 import kotlinx.android.synthetic.main.item_order.*
 
 /**
@@ -36,50 +33,12 @@ class OrderChildHolder(parent: ViewGroup?) : BaseItemViewHolder<Order>(parent, R
 
         if (data.state.contains(Order.OrderState.WAIT_PAY.name)) {
             pay_tv.visibility = View.VISIBLE
-            expired_time_tv.visibility = View.VISIBLE
-
-            if (data.countDownPaySeconds > 0) {
-                initCountDown(data.countDownPaySeconds)
-            }
-
         } else {
             pay_tv.visibility = View.GONE
-            expired_time_tv.visibility = View.GONE
         }
 
 
     }
 
-
-    private fun initCountDown(countDownPaySeconds: Long) {
-        val endTime = countDownPaySeconds
-        if (endTime <= 0) {
-            return
-        }
-
-        val millisInFuture: Long = endTime - System.currentTimeMillis()
-        if (millisInFuture <= 0) {
-            countdown_tv.visibility = View.GONE
-        } else {
-            countdown_tv.apply {
-                visibility = View.VISIBLE
-                cancel()
-                setTimeInFuture(SystemClock.elapsedRealtime() + millisInFuture)
-                setAutoDisplayText(true)
-                setTimeFormat(CountDownTextView.TIME_SHOW_D_H_M_S);
-                setModifierText(context.getString(R.string.expiration_time));
-                start()
-                addCountDownCallback(object : CountDownTextView.CountDownCallback {
-
-                    override fun onTick(countDownTextView: CountDownTextView?, millisUntilFinished: Long) = Unit
-                    override fun onFinish(countDownTextView: CountDownTextView?) {
-                        expired_time_tv.visibility = View.GONE
-                    }
-                })
-            }
-        }
-
-
-    }
 
 }
