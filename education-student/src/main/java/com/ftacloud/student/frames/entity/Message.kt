4322b647fe6ee35a550c90b4e9e9ba1ca983ef4c
wangshiwei <itshiwei@aliyun.com>
2020-11-05 10:18:25 +0800

    feat:消息pad适配

--- education-student/src/main/java/com/ftacloud/student/frames/entity/Message.kt
+++ education-student/src/main/java/com/ftacloud/student/frames/entity/Message.kt
@@ -6,4 +6,25 @@ package com.ftacloud.student.frames.entity
  *
  */
 class Message {
+
+    /**
+     * 消息头
+     */
+    var title = ""
+
+    /**
+     * 消息内容
+     */
+    var content = ""
+
+    /**
+     * 状态信息
+     */
+//    var state: Message.State? = null
+
+    /**
+     * 消息类型
+     */
+//    var type: Message.Type? = null
+
 }
