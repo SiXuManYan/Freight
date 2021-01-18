7c2f24c212b621c479aca9c1058cfe7dc7db1676
wangshiwei <itshiwei@aliyun.com>
2021-01-18 09:16:13 +0800

    refactor:code

--- education-student/src/main/java/com/fatcloud/account/ui/course/detail/prepare/NoClassActivity.kt
+++ education-student/src/main/java/com/fatcloud/account/ui/course/detail/prepare/NoClassActivity.kt
@@ -1,24 +1,35 @@
 package com.fatcloud.account.ui.course.detail.prepare
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.SystemClock
 import android.view.View
 import androidx.fragment.app.Fragment
+import butterknife.OnClick
+import com.baijiayun.groupclassui.InteractiveClassUI
+import com.baijiayun.livecore.utils.LPRxUtils
 import com.blankj.utilcode.constant.TimeConstants
 import com.blankj.utilcode.util.StringUtils
+import com.blankj.utilcode.util.ToastUtils
 import com.fatcloud.account.R
 import com.fatcloud.account.frames.components.BaseMVPActivity
 import com.fatcloud.account.frames.entity.FormalCourseDetail
+import com.fatcloud.account.frames.entity.home.Course
 import com.fatcloud.account.frames.entity.home.CourseState
 import com.fatcloud.account.frames.event.NoClassDataEvent
+import com.fatcloud.account.storage.entity.User
 import com.fatcloud.account.ui.course.detail.prepare.child.directory.DirectoryFragment
 import com.fatcloud.account.ui.course.detail.prepare.child.introduct.IntroductionFragment
 import com.fatcloud.account.ui.course.detail.prepare.child.teacher.TeacherFragment
+import com.fatcloud.account.ui.home.holder.CommonClassHolder
 import com.fatcloud.account.ui.order.list.OrderActivity
 import com.sugar.library.event.RxBus
 import com.sugar.library.ui.widget.countdown.CountDownTextView
+import com.sugar.library.util.CommonUtils
 import com.sugar.library.util.Constants
 import com.sugar.library.util.TimeUtil
 import kotlinx.android.synthetic.main.activity_no_class2.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by Wangsw on 2020/9/29 0029 11:15.
@@ -28,6 +39,7 @@ import kotlinx.android.synthetic.main.activity_no_class2.*
  */
 class NoClassActivity : BaseMVPActivity<NoClassPresenter>(), NoClassView {
 
+    private var mLiveRoomNo = ""
 
     companion object {
         internal val TAB_TITLES = arrayListOf(
@@ -49,8 +61,6 @@ class NoClassActivity : BaseMVPActivity<NoClassPresenter>(), NoClassView {
         pager.offscreenPageLimit = TAB_TITLES.size
         tabs_type.setViewPager(pager, TAB_TITLES.toTypedArray())
         initExtra()
-
-
     }
 
     private fun initExtra() {
@@ -64,59 +74,71 @@ class NoClassActivity : BaseMVPActivity<NoClassPresenter>(), NoClassView {
 
     override fun bindDetail(data: FormalCourseDetail) {
         RxBus.post(NoClassDataEvent(data))
+        mLiveRoomNo = data.liveRoomNo
+
         if (data.scheduleOuts.isNotEmpty()) {
+
             val scheduleOut = data.scheduleOuts[0]
 
             when (scheduleOut.scheduleState) {
-                CourseState.UNTEACH.name  -> {
+                CourseState.UNTEACH.name -> {
                     bottom_sw.displayedChild = 0
+                    val timeStrToSecond = TimeUtil.timeStrToSecond(scheduleOut.studyDatetime)
+                    initCountDown(timeStrToSecond)
                 }
-                CourseState.TEACHING.name->{
+                CourseState.TEACHING.name -> {
                     bottom_sw.displayedChild = 1
                 }
 
                 else -> {
                 }
             }
+        }
+    }
 
+    private fun initCountDown(endTime: Long) {
+        when {
+            endTime <= 0 -> {
+                count_down.text = "未开课"
+            }
+            endTime > TimeConstants.DAY -> {
+                count_down.text = "开课时间 " + TimeUtil.getDateTimeFromMillisecond(endTime)
+            }
+            else -> {
+                count_down.apply {
+                    cancel()
+                    setTimeInFuture(SystemClock.elapsedRealtime() + endTime)
+                    setAutoDisplayText(true)
+                    setTimeFormat(CountDownTextView.TIME_SHOW_D_H_M_S)
+                    start()
+                    addCountDownCallback(object : CountDownTextView.CountDownCallback {
 
+                        override fun onTick(countDownTextView: CountDownTextView?, millisUntilFinished: Long) = Unit
 
-            val timeStrToSecond = TimeUtil.timeStrToSecond(scheduleOut.studyDatetime)
-            initCountDown(timeStrToSecond)
+                        override fun onFinish(countDownTextView: CountDownTextView?) {
+                            bottom_sw.displayedChild = 1
+                        }
+                    })
+                }
+            }
         }
 
     }
 
-    private fun initCountDown(endTime: Long) {
-//        when {
-//            endTime <= 0 -> {
-//                countdown_tv.visibility = View.GONE
-//            }
-//            endTime > TimeConstants.DAY -> {
-//                count_down_ll.visibility = View.VISIBLE
-//                countdown_tv.text = TimeUtil.getDateTimeFromMillisecond(endTime)
-//            }
-//            else -> {
-//                countdown_tv.apply {
-//                    visibility = View.VISIBLE
-//                    cancel()
-//                    setTimeInFuture(SystemClock.elapsedRealtime() + endTime)
-//                    setAutoDisplayText(true)
-//                    setTimeFormat(CountDownTextView.TIME_SHOW_D_H_M_S)
-//                    start()
-//                    addCountDownCallback(object : CountDownTextView.CountDownCallback {
-//
-//                        override fun onTick(countDownTextView: CountDownTextView?, millisUntilFinished: Long) = Unit
-//
-//                        override fun onFinish(countDownTextView: CountDownTextView?) {
-//                            product_state_tv.text = StringUtils.getString(R.string.in_class)
-//                            count_down_ll.visibility = View.GONE
-//                        }
-//                    })
-//                }
-//            }
-//        }
+    @OnClick(
+        R.id.enter_tv
+    )
+    fun onClick(view: View) {
+        if (CommonUtils.isDoubleClick(view)) {
+            return
+        }
+        when (view.id) {
+            R.id.enter_tv -> {
 
+            }
+            else -> {
+            }
+        }
     }
 
 
@@ -155,5 +177,29 @@ class NoClassActivity : BaseMVPActivity<NoClassPresenter>(), NoClassView {
         override fun getCount() = OrderActivity.TAB_TITLES.size
     }
 
+
+    @SuppressLint("CheckResult")
+    fun enterLiveRoom(context: Context, view: View) {
+
+        LPRxUtils.clicks(view)
+            .throttleFirst(500, TimeUnit.MILLISECONDS)
+            .subscribe {
+
+
+                val code: String = mLiveRoomNo
+                if (code.isBlank()) {
+                    ToastUtils.showShort(R.string.classroom_not_exit)
+                    return@subscribe
+                }
+                var name = User.get().name
+                if (name.isNullOrBlank()) {
+                    name = "学生1"
+                }
+
+                InteractiveClassUI.enterRoom(context, code, name) { msg ->
+                    ToastUtils.showShort(msg)
+                }
+            }
+    }
 
 }
